8912491016b481b2b2c0607d4cf84de8fe252680
EminemBurger <yjg03298@naver.com>
2021-07-01 11:27:59 +0900

    port

--- client/js/champion.js
+++ client/js/champion.js
@@ -74,7 +74,7 @@ function ChampionAsset(data) {
 }
 
 
-fetch('http://carrygg-env-1.eba-e26mm6jp.ap-northeast-2.elasticbeanstalk.com:80/champion', {
+fetch('http://carrygg-env-1.eba-e26mm6jp.ap-northeast-2.elasticbeanstalk.com:8081/champion', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
